a563baa904c8c2686f436929e26c1bcd08a71275
ivan <ivan2000fly@gmail.com>
2024-06-07 15:47:40 +0500

    отредактировал класс продукта, добавил недостающие поля

--- flora_front_api_client/presentations/products.py
+++ flora_front_api_client/presentations/products.py
@@ -14,126 +14,6 @@ from .tags import Tag
 
 
 @dataclass
-class ProductBaseDataclass(BaseDataclass):
-    category_id: int = field(
-        metadata={
-            "strict": True,
-        }
-    )
-    name: str = field(metadata={"validate": Length(max=150, min=1)})
-    description: str | None = field(metadata={"validate": Length(max=1000)})
-    data: Any | None = field()
-    revision: int = field(
-        metadata={
-            "strict": True,
-        }
-    )
-    length: Decimal = field()
-    height: Decimal = field()
-    width: Decimal = field()
-    size_unit: str = field(metadata={"validate": OneOf([r.value for r in UnitOfSize])})
-    weight: Decimal = field()
-    weight_unit: str = field(
-        metadata={"validate": OneOf([r.value for r in UnitOfWeight])}
-    )
-
-
-@dataclass
-class ProductItem(BaseDataclass):
-    id: int | None = field()
-    product_id: int | None = field()
-    name: str = field(metadata={"validate": Length(max=150)})
-    cnt: Decimal = field()
-    unit: str = field(metadata={"validate": OneOf([r.value for r in UnitOfCount])})
-
-
-@dataclass
-class Product(ProductBaseDataclass):
-    id: int = field(
-        metadata={
-            "strict": True,
-        }
-    )
-    owner_id: int = field(
-        metadata={
-            "strict": True,
-        }
-    )
-    category: Category | None = field()
-    request_for_moderation: RequestForModeration | None = field()
-    type: ProductTypes = field()
-    is_template: bool = field(default=False)
-    tags: list[Tag] | None = field(default_factory=list)
-    images: list[Image] | None = field(default_factory=list)
-    items: list[ProductItem] | None = field(default_factory=list)
-
-    @property
-    def main_image(self) -> Image | None:
-        if not self.images:
-            return None
-        for i in self.images:
-            if i.position == 0:
-                return i
-
-    def __str__(self):
-        return f"#{self.id} {self.name}"
-
-
-@dataclass
-class FilesData(BaseDataclass):
-    id: int = field(
-        metadata={
-            "strict": True,
-        }
-    )
-    position: int = field(
-        metadata={
-            "strict": True,
-        }
-    )
-
-
-@dataclass
-class ProductRequest(ProductBaseDataclass):
-    tags: list[int] = field(default_factory=list, metadata={"required": True})
-    files: list[FilesData] = field(default_factory=list, metadata={"required": True})
-    items: list[ProductItem] = field(default_factory=list, metadata={"required": True})
-
-
-@dataclass
-class ProductResponse(SuccessResponse):
-    result: Product = field()
-
-
-@dataclass
-class ProductsResponse(PagedResponse):
-    result: list[Product] = field(default_factory=list, metadata={"required": True})
-
-
-@dataclass
-class FeaturedProductPrice(BaseDataclass):
-    currency: str = field(metadata={"validate": Length(equal=3)})
-    current: Decimal = field()
-    old: Decimal | None = field()
-    discount: Decimal = field()
-    discount_percent: int = field()
-    executor_discount_percent: int = field()
-    cashback: int = field()
-
-
-@dataclass
-class FeaturedProductExecutor(BaseDataclass):
-    id: int = field(
-        metadata={
-            "strict": True,
-        }
-    )
-    name: str = field(metadata={"validate": Length(max=150)})
-    url: str = field()
-    price: FeaturedProductPrice = field()
-
-
-@dataclass
 class Price:
     general: Decimal
     personal: Decimal
@@ -176,6 +56,124 @@ class DeliveryPrices:
     rub: Price
     eur: Price
     kzt: Price
+
+
+@dataclass
+class ProductBaseDataclass(BaseDataclass):
+    categories: list[Category]
+    name: str = field(metadata={"validate": Length(max=150, min=1)})
+    description: str | None = field(metadata={"validate": Length(max=1000)})
+    data: Any | None = field()
+    revision: int = field(
+        metadata={
+            "strict": True,
+        }
+    )
+    length: Decimal = field()
+    height: Decimal = field()
+    width: Decimal = field()
+    size_unit: str = field(metadata={"validate": OneOf([r.value for r in UnitOfSize])})
+    weight: Decimal = field()
+    weight_unit: str = field(
+        metadata={"validate": OneOf([r.value for r in UnitOfWeight])}
+    )
+    type: ProductTypes = field()
+    prices: Prices
+    delivery_prices: DeliveryPrices
+
+
+@dataclass
+class ProductItem(BaseDataclass):
+    id: int | None = field()
+    product_id: int | None = field()
+    name: str = field(metadata={"validate": Length(max=150)})
+    cnt: Decimal = field()
+    unit: str = field(metadata={"validate": OneOf([r.value for r in UnitOfCount])})
+
+
+@dataclass
+class FilesData(BaseDataclass):
+    id: int = field(
+        metadata={
+            "strict": True,
+        }
+    )
+    position: int = field(
+        metadata={
+            "strict": True,
+        }
+    )
+
+
+@dataclass
+class ProductRequest(ProductBaseDataclass):
+    tags: list[int] = field(default_factory=list, metadata={"required": True})
+    files: list[FilesData] = field(default_factory=list, metadata={"required": True})
+    items: list[ProductItem] = field(default_factory=list, metadata={"required": True})
+
+
+@dataclass
+class Product(ProductBaseDataclass):
+    id: int = field(
+        metadata={
+            "strict": True,
+        }
+    )
+    owner_id: int = field(
+        metadata={
+            "strict": True,
+        }
+    )
+
+    category: Category | None = field()
+    request_for_moderation: RequestForModeration | None = field()
+
+    is_template: bool = field(default=False)
+    images: list[Image] | None = field(default_factory=list)
+
+    @property
+    def main_image(self) -> Image | None:
+        if not self.images:
+            return None
+        for i in self.images:
+            if i.position == 0:
+                return i
+
+    def __str__(self):
+        return f"#{self.id} {self.name}"
+
+
+@dataclass
+class ProductResponse(SuccessResponse):
+    result: Product = field()
+
+
+@dataclass
+class ProductsResponse(PagedResponse):
+    result: list[Product] = field(default_factory=list, metadata={"required": True})
+
+
+@dataclass
+class FeaturedProductPrice(BaseDataclass):
+    currency: str = field(metadata={"validate": Length(equal=3)})
+    current: Decimal = field()
+    old: Decimal | None = field()
+    discount: Decimal = field()
+    discount_percent: int = field()
+    executor_discount_percent: int = field()
+    cashback: int = field()
+
+
+@dataclass
+class FeaturedProductExecutor(BaseDataclass):
+    id: int = field(
+        metadata={
+            "strict": True,
+        }
+    )
+    name: str = field(metadata={"validate": Length(max=150)})
+    url: str = field()
+    price: FeaturedProductPrice = field()
 
 
 @dataclass
@@ -270,3 +268,5 @@ class DeliveryTimePeriodRequest(BaseDataclass):
     city_id: int = field(metadata={"strict": True})
     delivery_date: date = field()
     ids: list[int] = field()
+
+
