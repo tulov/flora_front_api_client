265edb48017b814e20c27cd29dd64eb8e3b90b1d
lex <tulov.alex@gmail.com>
2024-06-29 16:42:23 +0500

    добавил к картинкам метод для получения адаптивной версии урла

--- flora_front_api_client/presentations/images.py
+++ flora_front_api_client/presentations/images.py
@@ -8,67 +8,38 @@ from .enums import ImageTarget
 import cloudinary
 
 
+def _init_cloudinary():
+    from ..client import FloraApiClient
+
+    cloudinary.config(
+        cloud_name=FloraApiClient.cloudinary_cloud_name,
+    )
+
+
 @dataclass
 class Image(BaseDataclass):
     id: str = field()
     provider: str = field()
 
     def build_url(self, *, width: int, height: int):
-        from ..client import FloraApiClient
-        cloudinary.config(
-            cloud_name=FloraApiClient.cloudinary_cloud_name,
-        )
+        _init_cloudinary()
         options = {
-            'width': width,
-            'height': height,
+            "width": width,
+            "height": height,
         }
-        url = cloudinary.CloudinaryImage(self.id).build_url(**options)
-        return url
+        return cloudinary.CloudinaryImage(self.id).build_url(**options)
 
     def full_url(self, *, width: int, height: int) -> str:
         return self.build_url(width=width, height=height)
 
-    @property
-    def mime_type(self) -> str:
-        if not self.path:
-            return ""
-        if self.path.endswith(".png"):
-            return "image/png"
-        if self.path.endswith(".jpeg"):
-            return "image/jpeg"
-        raise RuntimeError("Not supported file type")
-
-    @property
-    def extension(self) -> str:
-        if not self.mime_type:
-            return ""
-        m = self.mime_type
-        return m.split("/")[1]
-
-    def get_file_uploader_dict(
-        self,
-        file_width: int,
-        file_height: int,
-        url_width: int,
-        url_height: int,
-        thumb_width: int,
-        thumb_height: int,
-    ):
-        w = self.data.get("width", file_width)
-        h = self.data.get("height", file_height)
-        return {
-            "name": f"{self.id}.{self.extension}",
-            "type": f"image/{self.extension}",
-            "size": 0,
-            "file": self.full_url(width=w, height=h),
-            "local": "",
-            "data": {
-                "url": self.full_url(width=url_width, height=url_height),
-                "thumbnail": self.full_url(width=thumb_width, height=thumb_height),
-                "readerForce": True,
-                "listProps": {"id": self.id},
-            },
+    def adaptive_url(self) -> str:
+        _init_cloudinary()
+        options = {
+            "width": "auto",
+            "crop": "limit",
+            "dpr": "auto",
         }
+        return cloudinary.CloudinaryImage(self.id).build_url(**options)
 
 
 @dataclass
