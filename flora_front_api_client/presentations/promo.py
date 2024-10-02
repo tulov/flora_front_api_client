533e86650ec696651ac890b082a7edd78f1b002c
ivan <ivan2000fly@gmail.com>
2024-10-02 08:37:27 +0500

    Сделал функцию check для промо кодов, которая проверят действителен ли промо

--- flora_front_api_client/presentations/promo.py
+++ flora_front_api_client/presentations/promo.py
@@ -1,10 +1,14 @@
 from dataclasses import dataclass, field
+from datetime import datetime
 
 from marshmallow.validate import Length, OneOf
 
 from .base import SuccessResponse, BaseDataclass, PagedResponse
 from .enums import PromoTypes, PromoWorkPeriod
 
+
+def parse_date(date_str: str) -> datetime:
+    return datetime.strptime(date_str, "%d/%m")
 
 @dataclass
 class PromoCode(BaseDataclass):
@@ -17,6 +21,17 @@ class PromoCode(BaseDataclass):
     date_start: str | None = field(metadata={"validate": Length(max=15)}, default=None)
     date_end: str | None = field(metadata={"validate": Length(max=15)}, default=None)
 
+    def check(self) -> bool:
+        if not self.enabled:
+            return False
+        if self.work_days == "date":
+            if not parse_date(self.date_start) <= datetime.now() <= parse_date(self.date_end):
+                return False
+        elif self.work_days == "week":
+            days = list(map(int, self.day_of_week.split(',')))
+            current_day = datetime.now().weekday()
+            return current_day in days
+        return True
 
 @dataclass
 class PromoCodeResponse(SuccessResponse):
